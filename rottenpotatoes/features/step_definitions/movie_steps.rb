5b4250874baf5ef7d4f46a2872c9902bb44e531e
therealnabeel <nabeelsaleem@berkeley.edu>
2016-02-28 12:33:10 +0000

    Trying to pass autograder

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,3 +1,8 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create(movie)
